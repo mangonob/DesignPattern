8223e2e9232a8bd718e5c50e68394ac84ff48097
mangonob <534393806@qq.com>
2018-12-25 17:20:02 +0800

    + Chain of responsibility

---
+++ DesignPattern/ChainOfResponsibility.swift
@@ -0,0 +1,91 @@
+//
+//  ChainOfResponsibility.swift
+//  DesignPattern
+//
+//  Created by 高炼 on 2018/12/25.
+//  Copyright © 2018 BaiYiYuan. All rights reserved.
+//
+
+import Foundation
+
+class HelpHandler {
+    private weak var successor: HelpHandler?
+
+    init(_ helpHandler: HelpHandler? = nil) {
+        self.successor = helpHandler
+    }
+    
+    func hasHelp() -> Bool {
+        return true
+    }
+    
+    func handleHelp() {
+        self.successor?.handleHelp()
+    }
+    
+    func setHandler(_ helpHandler: HelpHandler?) {
+        self.successor = helpHandler
+    }
+}
+
+class Widget: HelpHandler {
+    var isEnable: Bool = true
+    private (set) var parent: Widget?
+    
+    init(_ parent: Widget?) {
+        super.init(parent)
+        self.parent = parent
+    }
+    
+    override init(_ helpHandler: HelpHandler?) {
+        super.init(helpHandler)
+    }
+
+    override func hasHelp() -> Bool {
+        return isEnable
+    }
+}
+
+class ButtonWidget: Widget {
+    override func handleHelp() {
+        guard hasHelp() else {
+            super.handleHelp()
+            return
+        }
+        
+        print("\(self) handle help at \(Date()).")
+    }
+}
+
+class Dialog: Widget {
+    override func handleHelp() {
+        guard hasHelp() else {
+            super.handleHelp()
+            return
+        }
+        
+        print("\(self) handle help at \(Date()).")
+    }
+}
+
+class Application: HelpHandler {
+    override func handleHelp() {
+        print("\(self) handle help at \(Date())")
+    }
+}
+
+struct ChainOfResponsibilityRoutine: Routine {
+    static func perform() {
+        let application = Application()
+        let dialog = Dialog(application)
+        let buttonWidget = ButtonWidget(dialog)
+        
+        buttonWidget.handleHelp()
+        
+        buttonWidget.isEnable = false
+        buttonWidget.handleHelp()
+        
+        dialog.isEnable = false
+        buttonWidget.handleHelp()
+    }
+}
--- DesignPattern/main.swift
+++ DesignPattern/main.swift
@@ -8,4 +8,4 @@
 
 import Foundation
 
-AdapterRoutine.perform()
+ChainOfResponsibilityRoutine.perform()
